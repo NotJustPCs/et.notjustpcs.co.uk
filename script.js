984f728ab8e3859ada5e119914f43d381cf2e3d7
Peter Fossey <peter@fossweb.net>
2018-07-14 12:20:29 +0100

    Update script.js
    Initial conversion from pt to et.

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ function getUrlParameter(name) {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-function WriteOneLink(URLprefix,URLsuffix,TelNo,FAicon,LinkDesc) {
-	return "<a class='list-group-item' target='_blank' href='" + URLprefix + TelNo + URLsuffix + "'><i class='fa fa-" + FAicon + " fa-fw' aria-hidden='true'></i>&nbsp; " + LinkDesc + "</a>";
+function WriteOneLink(URLprefix,URLsuffix,EmailAddress,FAicon,LinkDesc) {
+	return "<a class='list-group-item' target='_blank' href='" + URLprefix + EmailAddress + URLsuffix + "'><i class='fa fa-" + FAicon + " fa-fw' aria-hidden='true'></i>&nbsp; " + LinkDesc + "</a>";
 }
 
 function WriteLinks(IDtoWrite,LocalLinks) {
-	var PhoneNum = document.getElementById('PhoneNum').value;
-	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",PhoneNum,"facebook","Facebook");
-	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",PhoneNum + ' OR "' + PhoneNum.substring(0, 5) + " " + PhoneNum.substring(PhoneNum.length - 6) + '"',"google","Google");
-	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",PhoneNum.substring(PhoneNum.length - 10),"address-book-o","Google Contacts");
-	WLlinks = WLlinks + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",PhoneNum,"sitemap","Current supplier");
-	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",PhoneNum.substring(0, 5),"map-pin","Area code lookup");
-	WLlinks = WLlinks + WriteOneLink("tel:","",PhoneNum,"phone","Call number");
-	WLlinks = WLlinks + WriteOneLink("https://api.whatsapp.com/send?phone=44","",PhoneNum.substring(PhoneNum.length - 10),"whatsapp","Send WhatsApp message (UK numbers only)");
-	WLlinks = WLlinks + WriteOneLink("http://pt.notjustpcs.co.uk/?","",PhoneNum,"link","Link to this page");
+	var EmailAdd = document.getElementById('EmailAdd').value;
+	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",EmailAdd,"facebook","Facebook");
+	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",EmailAdd + ' OR "' + EmailAdd.substring(0, 5) + " " + EmailAdd.substring(EmailAdd.length - 6) + '"',"google","Google");
+	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",EmailAdd.substring(EmailAdd.length - 10),"address-book-o","Google Contacts");
+	WLlinks = WLlinks + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",EmailAdd,"sitemap","Current supplier");
+	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",EmailAdd.substring(0, 5),"map-pin","Area code lookup");
+	WLlinks = WLlinks + WriteOneLink("tel:","",EmailAdd,"phone","Call number");
+	WLlinks = WLlinks + WriteOneLink("https://api.whatsapp.com/send?phone=44","",EmailAdd.substring(EmailAdd.length - 10),"whatsapp","Send WhatsApp message (UK numbers only)");
+	WLlinks = WLlinks + WriteOneLink("http://et.notjustpcs.co.uk/?","",EmailAdd,"link","Link to this page");
 //	if (LocalLinks = 1) {
 //		WLlinks = WLlinks + WriteOneLink("http://njn/njpctools/","",PhoneNum,"archive","Internal NJPC Tools")		
 //	}
