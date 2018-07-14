9086c848bf57eddedc6b48fd9cafa9e81b344444
Peter Fossey <peter@fossweb.net>
2018-07-14 12:24:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,9 @@ function WriteOneLink(URLprefix,URLsuffix,EmailAddress,FAicon,LinkDesc) {
 function WriteLinks(IDtoWrite,LocalLinks) {
 	var EmailAdd = document.getElementById('EmailAdd').value;
 	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",EmailAdd,"facebook","Facebook");
-	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",EmailAdd + ' OR "' + EmailAdd.substring(0, 5) + " " + EmailAdd.substring(EmailAdd.length - 6) + '"',"google","Google");
-	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",EmailAdd.substring(EmailAdd.length - 10),"address-book-o","Google Contacts");
-	WLlinks = WLlinks + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",EmailAdd,"sitemap","Current supplier");
-	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",EmailAdd.substring(0, 5),"map-pin","Area code lookup");
-	WLlinks = WLlinks + WriteOneLink("tel:","",EmailAdd,"phone","Call number");
-	WLlinks = WLlinks + WriteOneLink("https://api.whatsapp.com/send?phone=44","",EmailAdd.substring(EmailAdd.length - 10),"whatsapp","Send WhatsApp message (UK numbers only)");
+	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",EmailAdd,"google","Google");
+	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",EmailAdd,"address-book-o","Google Contacts");
+	WLlinks = WLlinks + WriteOneLink("mailto:","",EmailAdd,"envelope","Send Message");
 	WLlinks = WLlinks + WriteOneLink("http://et.notjustpcs.co.uk/?","",EmailAdd,"link","Link to this page");
 //	if (LocalLinks = 1) {
 //		WLlinks = WLlinks + WriteOneLink("http://njn/njpctools/","",PhoneNum,"archive","Internal NJPC Tools")		
